5165e39e4f2a0ea615ea5c8f57243b73036cbd70
tatsuya0429 <ishimoto.t1247@gmail.com>
2022-02-14 15:55:14 +0900

    add todo model and repository

--- src/models/todo.go
+++ src/models/todo.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"github.com/edgedb/edgedb-go"
+)
+
+type Todo struct {
+	ID          edgedb.UUID
+	Title       string
+	Description string
+	Status      string
+	Deadline    edgedb.LocalDate
+	User        User
+}
--- src/repositories/todo_repository.go
+++ src/repositories/todo_repository.go
@@ -1,2 +1,45 @@
 package repositories
 
+import (
+	"context"
+
+	"github.com/edgedb/edgedb-go"
+	"github.com/tatsuya0429/edgedb_golang_docker_sample/src/infrastructure"
+	"github.com/tatsuya0429/edgedb_golang_docker_sample/src/models"
+)
+
+type TodoRepository struct{}
+
+func NewTodoRepository() *UserRepository {
+	return &UserRepository{}
+}
+
+func (repo *TodoRepository) GetAll(ctx context.Context) (todos []models.Todo, err error){
+	client := infrastructure.NewDBClient(ctx)
+	defer client.Close()
+	query := "SELECT Todo{id, title, status, deadline, user{id, username}}"
+	err = client.Query(ctx, query, &todos)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (repo *TodoRepository) CreateTodo(ctx context.Context, title string, description string, deadline string, userId string) error {
+	client := infrastructure.NewDBClient(ctx)
+	defer client.Close()
+	var inserted struct{ id edgedb.UUID }
+	query := `
+		INSERT Todo {
+			title := <str>$0, 
+			description := <str>$1,
+			deadline := <cal::local_date>$2,
+			user := (SELECT User FILTER .id = <uuid>$3)
+		}
+	`
+	err := client.QuerySingle(ctx, query, &inserted, title, description, deadline, userId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
